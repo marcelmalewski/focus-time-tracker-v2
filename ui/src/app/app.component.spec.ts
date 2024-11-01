d64f5ce201baaf62fe5a3e8d706df2ba37aa0809
Marcel Malewski <mmalewski9@gmail.com>
2024-11-01 16:31:04 +0100

    Created ui

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'focus-time-tracker-ui' title`, () => {
+  it(`should have the 'ui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('focus-time-tracker-ui');
+    expect(app.title).toEqual('ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, focus-time-tracker-ui');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ui');
   });
 });
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'focus-time-tracker-ui';
+  title = 'ui';
 }
