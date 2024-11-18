4e47c2884c6b5d546cd263507cdc8e2bd38609b8
Marcel Malewski <mmalewski9@gmail.com>
2024-11-18 20:29:56 +0100

    Updated login

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { TimerComponent } from './component/timer/timer.component';
+import { RegisterComponent } from './component/register/register.component';
+import { SettingsComponent } from './component/settings/settings.component';
 
 export const routes: Routes = [
-  { path: 'home', title: 'home', component: TimerComponent },
   { path: 'login', title: 'login', component: LoginComponent },
+  { path: 'register', title: 'register', component: RegisterComponent },
+  { path: 'timer', title: 'timer', component: TimerComponent },
+  { path: 'settings', title: 'settings', component: SettingsComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: LoginComponent }, //TODO add page not found
 ];
--- ui/src/app/component/timer/timer.component.ts
+++ ui/src/app/component/timer/timer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommandLineComponent } from '../command-line/command-line.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommandLineComponent],
   templateUrl: './timer.component.html',
 })
 export class TimerComponent {}
