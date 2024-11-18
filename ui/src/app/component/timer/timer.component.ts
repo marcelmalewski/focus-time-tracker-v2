3525175411abb84903804ee499756a9faaf5acff
Marcel Malewski <mmalewski9@gmail.com>
2024-11-18 20:51:52 +0100

    Updated

--- ui/src/app/component/timer/timer.component.ts
+++ ui/src/app/component/timer/timer.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommandLineComponent } from '../command-line/command-line.component';
+import { MatFormField, MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommandLineComponent],
+  imports: [CommandLineComponent, MatInput, MatFormField],
   templateUrl: './timer.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class TimerComponent {}
