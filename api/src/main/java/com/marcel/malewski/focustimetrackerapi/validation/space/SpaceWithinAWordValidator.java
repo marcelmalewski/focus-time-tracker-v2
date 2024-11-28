2ef1a0a863c94f6eb51f6d057dd28236af3d1d91
Marcel Malewski <mmalewski9@gmail.com>
2024-11-28 20:20:58 +0100

    Updated

---
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/validation/space/SpaceWithinAWordValidator.java
@@ -0,0 +1,20 @@
+package com.marcel.malewski.focustimetrackerapi.validation.space;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class SpaceWithinAWordValidator implements ConstraintValidator<WithoutSpacesWithinAWord, String> {
+	@Override
+	public boolean isValid(String string, ConstraintValidatorContext context) {
+		if (string == null || string.isBlank()) {
+			return true;
+		}
+
+		String trimmedString = string.trim();
+		if(trimmedString.contains(" ")) {
+			return false;
+		}
+
+		return true;
+	}
+}
---
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/validation/space/WithoutSpacesOnTheSides.java
@@ -0,0 +1,17 @@
+package com.marcel.malewski.focustimetrackerapi.validation.space;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = SpaceOnTheSidesValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WithoutSpacesOnTheSides {
+	String message() default "must not contain spaces on the sides";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+}
+
