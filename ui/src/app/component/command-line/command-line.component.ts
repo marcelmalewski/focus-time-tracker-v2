9ab17971bd1b40dfc2654ea05e28f00941295e99
Marcel Malewski <mmalewski9@gmail.com>
2025-01-14 20:45:26 +0100

    Formatted

--- ui/src/app/component/command-line/command-line.component.ts
+++ ui/src/app/component/command-line/command-line.component.ts
@@ -1,113 +1,119 @@
 import {
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  ElementRef,
-  HostListener,
-  Input,
-  OnInit,
-  ViewChild,
+    Component,
+    CUSTOM_ELEMENTS_SCHEMA,
+    ElementRef,
+    HostListener,
+    Input,
+    OnInit,
+    ViewChild,
 } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Router } from '@angular/router';
 import {
-  MatAutocomplete,
-  MatAutocompleteTrigger,
-  MatOption,
+    MatAutocomplete,
+    MatAutocompleteTrigger,
+    MatOption,
 } from '@angular/material/autocomplete';
-import { map, Observable, startWith } from 'rxjs';
+import { map, Observable, startWith, tap } from 'rxjs';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import {
-  Command,
-  Commands,
-  CommandsValues,
-  Pages,
+    Command,
+    Commands,
+    CommandsValues,
+    Pages,
 } from '../../other/typesAndConsts';
 
 @Component({
-  selector: 'app-command-line',
-  standalone: true,
-  imports: [
-    FormsModule,
-    NgIf,
-    MatAutocompleteTrigger,
-    MatAutocomplete,
-    MatOption,
-    MatFormField,
-    MatInput,
-    ReactiveFormsModule,
-    AsyncPipe,
-  ],
-  templateUrl: './command-line.component.html',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    selector: 'app-command-line',
+    standalone: true,
+    imports: [
+        FormsModule,
+        NgIf,
+        MatAutocompleteTrigger,
+        MatAutocomplete,
+        MatOption,
+        MatFormField,
+        MatInput,
+        ReactiveFormsModule,
+        AsyncPipe,
+    ],
+    templateUrl: './command-line.component.html',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CommandLineComponent implements OnInit {
-  @Input({ required: true })
-  commandContext!: string;
+    @Input({ required: true })
+    commandContext!: string;
 
-  commandInputControl = new FormControl('');
-  commands: Command[] = CommandsValues;
-  filteredCommands: Observable<string[]> | undefined;
-  firstCommandFromFilteredCommands: string | undefined;
+    isAutocompleteDisabled = true;
+    commandInputControl = new FormControl('');
+    commands: Command[] = CommandsValues;
+    filteredCommands: Observable<string[]> | undefined;
+    firstCommandFromFilteredCommands: string | undefined;
 
-  @ViewChild('commandInput', { static: true })
-  commandInput!: ElementRef<HTMLInputElement>;
+    @ViewChild('commandInput', { static: true })
+    commandInput!: ElementRef<HTMLInputElement>;
 
-  constructor(private router: Router) {}
+    constructor(private router: Router) {}
 
-  ngOnInit() {
-    this.filteredCommands = this.commandInputControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this.filterCommands(value || ''))
-    );
-  }
+    ngOnInit() {
+        this.filteredCommands = this.commandInputControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this.filterCommands(value || ''))
+        );
 
-  private filterCommands(value: string): string[] {
-    if (value === '') {
-      return [];
+        this.filteredCommands.subscribe(commands => {
+            this.isAutocompleteDisabled = commands.length === 0;
+        });
     }
 
-    const filterValue = value.toLowerCase();
-    const filteredCommands = this.commands.filter(option =>
-      option.toLowerCase().includes(filterValue)
-    );
-    this.firstCommandFromFilteredCommands = filteredCommands[0];
+    private filterCommands(value: string): string[] {
+        if (value === '') {
+            return [];
+        }
 
-    return filteredCommands;
-  }
+        const filterValue = value.toLowerCase();
+        const filteredCommands = this.commands.filter(option =>
+            option.toLowerCase().includes(filterValue)
+        );
+        this.firstCommandFromFilteredCommands = filteredCommands[0];
 
-  onSubmit() {
-    const commandValue: string | null = this.firstCommandFromFilteredCommands
-      ? this.firstCommandFromFilteredCommands
-      : this.commandInputControl.value;
-    if (!commandValue) {
-      return;
+        return filteredCommands;
     }
 
-    if (commandValue === Commands.GO_TO_SETTINGS) {
-      this.router.navigate([Pages.SETTINGS]);
-    } else {
-      this.router.navigate([Pages.TIMER]);
+    onSubmit() {
+        const commandValue: string | null = this
+            .firstCommandFromFilteredCommands
+            ? this.firstCommandFromFilteredCommands
+            : this.commandInputControl.value;
+        if (!commandValue) {
+            return;
+        }
+
+        if (commandValue === Commands.GO_TO_SETTINGS) {
+            this.router.navigate([Pages.SETTINGS]);
+        } else {
+            this.router.navigate([Pages.TIMER]);
+        }
+
+        this.commandInputControl.reset();
     }
 
-    this.commandInputControl.reset();
-  }
-
-  @HostListener('document:keydown', ['$event'])
-  handleShortcut(event: KeyboardEvent): void {
-    if (
-      event.key === 'c' &&
-      !event.ctrlKey &&
-      !event.altKey &&
-      !event.metaKey
-    ) {
-      event.preventDefault();
-      this.focusInput();
+    @HostListener('document:keydown', ['$event'])
+    handleShortcut(event: KeyboardEvent): void {
+        if (
+            event.key === 'c' &&
+            !event.ctrlKey &&
+            !event.altKey &&
+            !event.metaKey
+        ) {
+            event.preventDefault();
+            this.focusInput();
+        }
     }
-  }
 
-  private focusInput(): void {
-    this.commandInput.nativeElement.focus();
-  }
+    private focusInput(): void {
+        this.commandInput.nativeElement.focus();
+    }
 }
