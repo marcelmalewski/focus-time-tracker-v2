23a76db63935ee12bd5de47aa94ff8b3c5e3f8a6
Marcel Malewski <mmalewski9@gmail.com>
2025-01-19 12:01:50 +0100

    Added logout button

--- ui/src/app/component/timer/timer.component.ts
+++ ui/src/app/component/timer/timer.component.ts
@@ -1,12 +1,63 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnDestroy } from '@angular/core';
 import { CommandLineComponent } from '../command-line/command-line.component';
 import { MatFormField, MatInput } from '@angular/material/input';
+import { HttpParams, HttpResponse } from '@angular/common/http';
+import { Subject, takeUntil } from 'rxjs';
+import { Pages } from '../../other/typesAndConsts';
+import { GeneralActionsService } from '../../service/general-actions.service';
+import { Router } from '@angular/router';
+import { PrincipalDataService } from '../../service/principal-data.service';
+import {
+    LoggedOutMessage,
+    UnknownServerErrorMessage,
+} from '../../other/message';
+import { NotificationService } from '../../service/notification.service';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommandLineComponent, MatInput, MatFormField],
-  templateUrl: './timer.component.html',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommandLineComponent, MatInput, MatFormField],
+    templateUrl: './timer.component.html',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class TimerComponent {}
+export class TimerComponent implements OnDestroy {
+    private componentDestroyed$ = new Subject<void>();
+
+    constructor(
+        private router: Router,
+        private generalActionsService: GeneralActionsService,
+        private principalDataService: PrincipalDataService,
+        private notificationService: NotificationService
+    ) {}
+
+    ngOnDestroy(): void {
+        this.componentDestroyed$.next();
+        this.componentDestroyed$.complete();
+    }
+
+    submitLogout() {
+        this.generalActionsService
+            .logout()
+            .pipe(takeUntil(this.componentDestroyed$))
+            .subscribe({
+                error: (response: HttpResponse<any>) => {
+                    if (response.status === 401) {
+                        this.principalDataService.setPrincipalBasicData(
+                            undefined
+                        );
+                        this.router.navigateByUrl(Pages.LOGIN);
+                        this.notificationService.openSuccessNotification(
+                            LoggedOutMessage
+                        );
+                    } else {
+                        this.notificationService.openErrorNotification(
+                            UnknownServerErrorMessage
+                        );
+                        this.router.navigateByUrl(Pages.UNKNOWN_ERROR, {
+                            skipLocationChange: true,
+                        });
+                    }
+                },
+            });
+    }
+}
