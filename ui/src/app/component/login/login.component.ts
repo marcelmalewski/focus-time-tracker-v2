4b653daa0a5ea76e6a3f041bf869f04ab123d975
Marcel Malewski <mmalewski9@gmail.com>
2025-01-19 11:18:58 +0100

    Formatted

--- ui/src/app/component/login/login.component.ts
+++ ui/src/app/component/login/login.component.ts
@@ -6,59 +6,58 @@ import { Subject, takeUntil } from 'rxjs';
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Pages } from '../../other/typesAndConsts';
 import { Router } from '@angular/router';
+import { GeneralActionsService } from '../../service/general-actions.service';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule, NgIf],
-  templateUrl: './login.component.html',
+    selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule, NgIf],
+    templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnDestroy {
-  @ViewChild('loginForm') private loginForm!: NgForm;
-  private componentDestroyed$ = new Subject<void>();
-  notCorrectLoginOrEmailOrPasswords: boolean = false;
+    @ViewChild('loginForm') private loginForm!: NgForm;
+    private componentDestroyed$ = new Subject<void>();
+    notCorrectLoginOrEmailOrPasswords: boolean = false;
 
-  loginData: AuthInterface = {
-    loginOrEmail: '',
-    password: '',
-  };
+    loginData: AuthInterface = {
+        loginOrEmail: '',
+        password: '',
+    };
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) {}
+    constructor(
+        private router: Router,
+        private generalActionsService: GeneralActionsService
+    ) {}
 
-  ngOnDestroy(): void {
-    this.componentDestroyed$.next();
-    this.componentDestroyed$.complete();
-  }
-
-  onSubmit() {
-    if (this.loginForm.invalid) {
-      return;
+    ngOnDestroy(): void {
+        this.componentDestroyed$.next();
+        this.componentDestroyed$.complete();
     }
 
-    this.notCorrectLoginOrEmailOrPasswords = false;
-    const payload = new HttpParams()
-      .set('username', this.loginData.loginOrEmail)
-      .set('password', this.loginData.password);
+    onSubmit() {
+        if (this.loginForm.invalid) {
+            return;
+        }
 
-    this.http
-      .post('api/login', payload)
-      .pipe(takeUntil(this.componentDestroyed$))
-      .subscribe({
-        next: () => {
-          this.router.navigateByUrl(Pages.TIMER);
-        },
-        error: (response: HttpResponse<any>) => {
-          console.log(response.status);
-          console.log('response.status');
-          if (response.status === 401) {
-            this.notCorrectLoginOrEmailOrPasswords = true;
-          } else {
-            // TODO inny error?
-          }
-        },
-      });
-  }
+        this.notCorrectLoginOrEmailOrPasswords = false;
+        const payload = new HttpParams()
+            .set('username', this.loginData.loginOrEmail)
+            .set('password', this.loginData.password);
+
+        this.generalActionsService
+            .login(payload)
+            .pipe(takeUntil(this.componentDestroyed$))
+            .subscribe({
+                next: () => {
+                    this.router.navigateByUrl(Pages.TIMER);
+                },
+                error: (response: HttpResponse<any>) => {
+                    if (response.status === 401) {
+                        this.notCorrectLoginOrEmailOrPasswords = true;
+                    } else {
+                        // TODO inny error?
+                    }
+                },
+            });
+    }
 }
