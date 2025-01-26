fe8e2b97b8bb40d47044305c67493e6f5b572cca
Marcel Malewski <mmalewski9@gmail.com>
2025-01-26 11:05:40 +0100

    Formatted

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/PersonRepository.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/PersonRepository.java
@@ -12,85 +12,85 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
-  @Query("SELECT person FROM Person person " +
-    "JOIN FETCH person.mainTopics mainTopics Where person.id = ?1 ")
-  Optional<Person> findByIdWithFetchedMainTopics(Long id);
+    @Query("SELECT person FROM Person person " +
+        "JOIN FETCH person.mainTopics mainTopics Where person.id = ?1 ")
+    Optional<Person> findByIdWithFetchedMainTopics(Long id);
 
-  Optional<Person> findByLoginOrEmail(String login, String email);
+    Optional<Person> findByLoginOrEmail(String login, String email);
 
-  boolean existsByLogin(String login);
+    boolean existsByLogin(String login);
 
-  boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
 
-  @Modifying(clearAutomatically = true)
-  @Query("UPDATE Person person SET person.timerStage = :timerStage WHERE person.id = :id")
-  @Transactional
-  int updateTimerStage(@Param(value = "id") long id, @Param(value = "timerStage") Stage timerStage);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Person person SET person.timerStage = :timerStage WHERE person.id = :id")
+    @Transactional
+    int updateTimerStage(@Param(value = "id") long id, @Param(value = "timerStage") Stage timerStage);
 
-  @Modifying(clearAutomatically = true)
-  @Query("UPDATE Person person SET person.timerAutoBreak = :timerAutoBreak WHERE person.id = :id")
-  @Transactional
-  int updateTimerAutoBreak(@Param(value = "id") long id, @Param(value = "timerAutoBreak") boolean timerAutoBreak);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Person person SET person.timerAutoBreak = :timerAutoBreak WHERE person.id = :id")
+    @Transactional
+    int updateTimerAutoBreak(@Param(value = "id") long id, @Param(value = "timerAutoBreak") boolean timerAutoBreak);
 
-  @Modifying(clearAutomatically = true)
-  @Query("UPDATE Person person " +
-    "SET person.timerStage = :timerStage," +
-    "person.timerRemainingTime = :timerRemainingTime " +
-    "WHERE person.id = :id")
-  @Transactional
-  int updateTimerStageAndRemainingTime(
-    @Param(value = "id") long id,
-    @Param(value = "timerStage") Stage timerStage,
-    @Param(value = "timerRemainingTime") int timerRemainingTime
-  );
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Person person " +
+        "SET person.timerStage = :timerStage," +
+        "person.timerRemainingTime = :timerRemainingTime " +
+        "WHERE person.id = :id")
+    @Transactional
+    int updateTimerStageAndRemainingTime(
+        @Param(value = "id") long id,
+        @Param(value = "timerStage") Stage timerStage,
+        @Param(value = "timerRemainingTime") int timerRemainingTime
+    );
 
-  @Modifying(clearAutomatically = true)
-  @Query("UPDATE Person person " +
-    "SET person.timerSelectedTopic = :timerSelectedTopic," +
-    "person.timerSetHours = :timerSetHours," +
-    "person.timerSetMinutes = :timerSetMinutes," +
-    "person.timerSetSeconds = :timerSetSeconds," +
-    "person.timerShortBreak = :timerShortBreak," +
-    "person.timerLongBreak = :timerLongBreak," +
-    "person.timerInterval = :timerInterval," +
-    "person.timerStage = :timerStage," +
-    "person.timerRemainingTime = :timerRemainingTime " +
-    "WHERE person.id = :id")
-  @Transactional
-  int updatePrincipalFocusAfterHomeWithAutoBreakOn(
-    @Param(value = "id") long id,
-    @Param(value = "timerSelectedTopic") String timerSelectedTopic,
-    @Param(value = "timerSetHours") Integer hours,
-    @Param(value = "timerSetMinutes") Integer minutes,
-    @Param(value = "timerSetSeconds") Integer seconds,
-    @Param(value = "timerShortBreak") Integer shortBreak,
-    @Param(value = "timerLongBreak") Integer longBreak,
-    @Param(value = "timerInterval") Integer interval,
-    @Param(value = "timerStage") Stage timerStage,
-    @Param(value = "timerRemainingTime") Integer remainingTime
-  );
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Person person " +
+        "SET person.timerSelectedTopic = :timerSelectedTopic," +
+        "person.timerSetHours = :timerSetHours," +
+        "person.timerSetMinutes = :timerSetMinutes," +
+        "person.timerSetSeconds = :timerSetSeconds," +
+        "person.timerShortBreak = :timerShortBreak," +
+        "person.timerLongBreak = :timerLongBreak," +
+        "person.timerInterval = :timerInterval," +
+        "person.timerStage = :timerStage," +
+        "person.timerRemainingTime = :timerRemainingTime " +
+        "WHERE person.id = :id")
+    @Transactional
+    int updatePrincipalFocusAfterHomeWithAutoBreakOn(
+        @Param(value = "id") long id,
+        @Param(value = "timerSelectedTopic") String timerSelectedTopic,
+        @Param(value = "timerSetHours") Integer hours,
+        @Param(value = "timerSetMinutes") Integer minutes,
+        @Param(value = "timerSetSeconds") Integer seconds,
+        @Param(value = "timerShortBreak") Integer shortBreak,
+        @Param(value = "timerLongBreak") Integer longBreak,
+        @Param(value = "timerInterval") Integer interval,
+        @Param(value = "timerStage") Stage timerStage,
+        @Param(value = "timerRemainingTime") Integer remainingTime
+    );
 
-  @Modifying(clearAutomatically = true)
-  @Query("UPDATE Person person " +
-    "SET person.timerSelectedTopic = :timerSelectedTopic," +
-    "person.timerSetHours = :timerSetHours," +
-    "person.timerSetMinutes = :timerSetMinutes," +
-    "person.timerSetSeconds = :timerSetSeconds," +
-    "person.timerShortBreak = :timerShortBreak," +
-    "person.timerLongBreak = :timerLongBreak," +
-    "person.timerStage = :timerStage," +
-    "person.timerRemainingTime = :timerRemainingTime " +
-    "WHERE person.id = :id")
-  @Transactional
-  int updatePrincipalFocusAfterHomeWithAutoBreakOff(
-    @Param(value = "id") long id,
-    @Param(value = "timerSelectedTopic") String timerSelectedTopic,
-    @Param(value = "timerSetHours") Integer hours,
-    @Param(value = "timerSetMinutes") Integer minutes,
-    @Param(value = "timerSetSeconds") Integer seconds,
-    @Param(value = "timerShortBreak") Integer shortBreak,
-    @Param(value = "timerLongBreak") Integer longBreak,
-    @Param(value = "timerStage") Stage timerStage,
-    @Param(value = "timerRemainingTime") Integer remainingTime
-  );
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Person person " +
+        "SET person.timerSelectedTopic = :timerSelectedTopic," +
+        "person.timerSetHours = :timerSetHours," +
+        "person.timerSetMinutes = :timerSetMinutes," +
+        "person.timerSetSeconds = :timerSetSeconds," +
+        "person.timerShortBreak = :timerShortBreak," +
+        "person.timerLongBreak = :timerLongBreak," +
+        "person.timerStage = :timerStage," +
+        "person.timerRemainingTime = :timerRemainingTime " +
+        "WHERE person.id = :id")
+    @Transactional
+    int updatePrincipalFocusAfterHomeWithAutoBreakOff(
+        @Param(value = "id") long id,
+        @Param(value = "timerSelectedTopic") String timerSelectedTopic,
+        @Param(value = "timerSetHours") Integer hours,
+        @Param(value = "timerSetMinutes") Integer minutes,
+        @Param(value = "timerSetSeconds") Integer seconds,
+        @Param(value = "timerShortBreak") Integer shortBreak,
+        @Param(value = "timerLongBreak") Integer longBreak,
+        @Param(value = "timerStage") Stage timerStage,
+        @Param(value = "timerRemainingTime") Integer remainingTime
+    );
 }
