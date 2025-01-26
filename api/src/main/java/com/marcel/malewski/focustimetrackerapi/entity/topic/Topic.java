2cd435d9b1f4632d271710d8b1caa5bf62d7844c
Marcel Malewski <mmalewski9@gmail.com>
2025-01-26 11:05:01 +0100

    Refactored structure

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/topic/Topic.java
+++
@@ -1,7 +0,0 @@
-package com.marcel.malewski.focustimetrackerapi.entity.topic;
-
-public interface Topic {
-  String getName();
-
-  void setName(String name);
-}
---
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/topic/interfaces/Topic.java
@@ -0,0 +1,7 @@
+package com.marcel.malewski.focustimetrackerapi.entity.topic.interfaces;
+
+public interface Topic {
+    String getName();
+
+    void setName(String name);
+}
