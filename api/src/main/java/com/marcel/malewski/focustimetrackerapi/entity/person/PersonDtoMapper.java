b4ab17430a4e655249ba6149b80d2d677f20e334
Marcel Malewski <mmalewski9@gmail.com>
2025-01-28 20:33:55 +0100

    Updated person mapper

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/PersonDtoMapper.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/PersonDtoMapper.java
@@ -6,33 +6,33 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public abstract class PersonDtoMapper {
-    public TimerPauseDto toTimerPauseDto(PrincipalWithMainTopicsDto principalWithMainTopicsDto) {
-        if (principalWithMainTopicsDto == null) {
-            return null;
-        }
-
-        String setTimePretty = principalWithMainTopicsDto.timerSetHours() + "h " +
-            principalWithMainTopicsDto.timerSetMinutes() + "m " +
-            principalWithMainTopicsDto.timerSetSeconds() + "s";
-
-        int remainingTime = principalWithMainTopicsDto.timerRemainingTime();
-        int remainingTimeSeconds = remainingTime % 60;
-        int remainingTimeMinutes = (remainingTime / 60) % 60;
-        int remainingTimeHours = remainingTime / 60 / 60;
-
-        String remainingTimePretty = remainingTimeHours + "h " +
-            remainingTimeMinutes + "m " +
-            remainingTimeSeconds + "s";
-
-
-        return new TimerPauseDto(
-            setTimePretty,
-            remainingTimePretty,
-            principalWithMainTopicsDto.timerRemainingTime(),
-            principalWithMainTopicsDto.timerSelectedTopic(),
-            principalWithMainTopicsDto.timerShortBreak(),
-            principalWithMainTopicsDto.timerLongBreak(),
-            principalWithMainTopicsDto.timerAutoBreak()
-        );
-    }
+//    public TimerPauseDto toTimerPauseDto(PrincipalWithMainTopicsDto principalWithMainTopicsDto) {
+//        if (principalWithMainTopicsDto == null) {
+//            return null;
+//        }
+//
+//        String setTimePretty = principalWithMainTopicsDto.timerSetHours() + "h " +
+//            principalWithMainTopicsDto.timerSetMinutes() + "m " +
+//            principalWithMainTopicsDto.timerSetSeconds() + "s";
+//
+//        int remainingTime = principalWithMainTopicsDto.timerRemainingTime();
+//        int remainingTimeSeconds = remainingTime % 60;
+//        int remainingTimeMinutes = (remainingTime / 60) % 60;
+//        int remainingTimeHours = remainingTime / 60 / 60;
+//
+//        String remainingTimePretty = remainingTimeHours + "h " +
+//            remainingTimeMinutes + "m " +
+//            remainingTimeSeconds + "s";
+//
+//
+//        return new TimerPauseDto(
+//            setTimePretty,
+//            remainingTimePretty,
+//            principalWithMainTopicsDto.timerRemainingTime(),
+//            principalWithMainTopicsDto.timerSelectedTopic(),
+//            principalWithMainTopicsDto.timerShortBreak(),
+//            principalWithMainTopicsDto.timerLongBreak(),
+//            principalWithMainTopicsDto.timerAutoBreak()
+//        );
+//    }
 }
--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/PersonMapper.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/PersonMapper.java
@@ -1,12 +1,9 @@
 package com.marcel.malewski.focustimetrackerapi.entity.person;
 
 import com.marcel.malewski.focustimetrackerapi.entity.person.dto.PrincipalBasicDataDto;
-import com.marcel.malewski.focustimetrackerapi.entity.person.dto.PrincipalWithMainTopicsDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
     PrincipalBasicDataDto toPrincipalBasicDataDto(Person person);
-
-    PrincipalWithMainTopicsDto toPrincipalBasicDataWithMainTopicsDto(Person person);
 }
