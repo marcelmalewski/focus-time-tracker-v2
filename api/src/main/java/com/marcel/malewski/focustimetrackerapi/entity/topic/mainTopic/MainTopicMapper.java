9c29e5caf19d7cf250842073a710b9826d05bda0
Marcel Malewski <mmalewski9@gmail.com>
2025-01-28 20:33:42 +0100

    Updated main topic mapper

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/topic/mainTopic/MainTopicMapper.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/topic/mainTopic/MainTopicMapper.java
@@ -3,9 +3,7 @@ package com.marcel.malewski.focustimetrackerapi.entity.topic.mainTopic;
 import com.marcel.malewski.focustimetrackerapi.entity.topic.dto.TopicBasicDataDto;
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface MainTopicMapper {
-    List<TopicBasicDataDto> toMainTopicsBasicDataDto(List<MainTopic> mainTopics);
+    TopicBasicDataDto toMainTopicsBasicDataDto(MainTopic mainTopics);
 }
