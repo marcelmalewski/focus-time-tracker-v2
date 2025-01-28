8afe429351fd29139451887c54cec925afa01c8e
Marcel Malewski <mmalewski9@gmail.com>
2025-01-28 20:34:14 +0100

    Updated topic objects

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalWithMainTopicsDto.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalWithMainTopicsDto.java
@@ -1,27 +1,14 @@
 package com.marcel.malewski.focustimetrackerapi.entity.person.dto;
 
+import com.marcel.malewski.focustimetrackerapi.entity.person.interfaces.PrincipalBasicData;
 import com.marcel.malewski.focustimetrackerapi.entity.person.interfaces.PrincipalWithMainTopics;
-import com.marcel.malewski.focustimetrackerapi.entity.topic.mainTopic.MainTopicBasicDataDto;
-import com.marcel.malewski.focustimetrackerapi.enums.Stage;
+import com.marcel.malewski.focustimetrackerapi.entity.topic.interfaces.TopicBasicData;
 
 import java.util.List;
 
 // TODO walidacja
 public record PrincipalWithMainTopicsDto(
-    Long id,
-    String login,
-    String email,
-    Stage timerStage,
-    String timerSelectedTopic,
-    Integer timerSetHours,
-    Integer timerSetMinutes,
-    Integer timerSetSeconds,
-    Integer timerShortBreak,
-    Integer timerLongBreak,
-    Boolean timerAutoBreak,
-    Boolean stopWatchAutoBreak,
-    Integer timerRemainingTime,
-    Integer timerInterval,
-    List<MainTopicBasicDataDto> mainTopicsBasicDataDtos
+    PrincipalBasicData principalBasicData,
+    List<TopicBasicData> mainTopicsBasicData
 ) implements PrincipalWithMainTopics {
 }
--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/interfaces/PrincipalWithMainTopics.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/interfaces/PrincipalWithMainTopics.java
@@ -1,9 +1,11 @@
 package com.marcel.malewski.focustimetrackerapi.entity.person.interfaces;
 
-import com.marcel.malewski.focustimetrackerapi.entity.topic.mainTopic.MainTopicBasicDataDto;
+import com.marcel.malewski.focustimetrackerapi.entity.topic.interfaces.TopicBasicData;
 
 import java.util.List;
 
-public interface PrincipalWithMainTopics extends PrincipalBasicData {
-    List<MainTopicBasicDataDto> mainTopicsBasicDataDtos();
+public interface PrincipalWithMainTopics {
+    PrincipalBasicData principalBasicData();
+
+    List<TopicBasicData> mainTopicsBasicData();
 }
