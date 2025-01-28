e880c150ded6f2ae325f781f26acf66128f59164
Marcel Malewski <mmalewski9@gmail.com>
2025-01-28 20:44:11 +0100

    Fixed person interfaces

--- ui/src/app/interface/person.interface.ts
+++ ui/src/app/interface/person.interface.ts
@@ -1,18 +1,28 @@
 import { Stage } from '../other/typesAndConsts';
 
 export interface PrincipalBasicData {
-  id: number;
-  login: string;
-  email: string;
-  timerStage: Stage;
-  timerSelectedTopic: string;
-  timerSetHours: number;
-  timerSetMinutes: number;
-  timerSetSeconds: number;
-  timerShortBreak: number;
-  timerLongBreak: number;
-  timerAutoBreak: boolean;
-  stopWatchAutoBreak: boolean;
-  timerRemainingTime: number;
-  timerInterval: number;
+    id: number;
+    login: string;
+    email: string;
+    timerStage: Stage;
+    timerSelectedTopic: string;
+    timerSetHours: number;
+    timerSetMinutes: number;
+    timerSetSeconds: number;
+    timerShortBreak: number;
+    timerLongBreak: number;
+    timerAutoBreak: boolean;
+    stopWatchAutoBreak: boolean;
+    timerRemainingTime: number;
+    timerInterval: number;
+}
+
+export interface PrincipalWithMainTopics {
+    principalBasicData: PrincipalBasicData;
+    mainTopicsBasicData: MainTopicBasicData[];
+}
+
+export interface MainTopicBasicData {
+    id: number;
+    name: string;
 }
