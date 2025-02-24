703fee4ea23f9787e469c5740a57d45e7ca2623f
Marcel Malewski <mmalewski9@gmail.com>
2025-02-24 20:55:09 +0100

    Added new message

--- ui/src/app/other/message.ts
+++ ui/src/app/other/message.ts
@@ -1,9 +1,12 @@
 export const NotLoggedInMessage = "You can't enter, you are not logged in.";
 export const LoggedOutMessage = 'Logged out successfully.';
-export const UnknownServerErrorMessage =
+export const UnknownServerErrorMessageRefreshPage =
     'Something went wrong, try refreshing page.';
+export const UnknownServerErrorMessage = 'Something went wrong.';
 
 export const AtLeastZeroMessage = 'Must be at least 0';
 export const AtLeastOneMessage = 'Must be at least 1';
 export const LessThanOrEqual59Message = 'Must be less than or equal to 59';
 export const LessThanOrEqual99Message = 'Must be less than or equal to 99';
+
+export const TimerSettingsUpdated = 'Updated timer settings successfully';
