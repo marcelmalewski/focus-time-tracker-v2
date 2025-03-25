167cf37cdf2ed4dbb8e0f6b8f27ac52b29873ffa
Marcel Malewski <mmalewski9@gmail.com>
2025-03-25 20:06:46 +0100

    Removed useless class

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/UpdateTimerAutoBreakDto.java
+++
@@ -1,5 +0,0 @@
-package com.marcel.malewski.focustimetrackerapi.entity.person.dto;
-
-// TODO walidacja?
-public record UpdateTimerAutoBreakDto(String timerAutoBreakInput, Integer timerInterval) {
-}
