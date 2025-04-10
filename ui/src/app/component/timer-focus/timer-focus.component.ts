d38537c2afaca91e8731f7f1368ba5620e73ebdb
Marcel Malewski <mmalewski9@gmail.com>
2025-04-10 20:45:02 +0200

    Added timer focus

---
+++ ui/src/app/component/timer-focus/timer-focus.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timer-focus',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer-focus.component.html',
+  styleUrl: './timer-focus.component.css'
+})
+export class TimerFocusComponent {
+
+}
