f297bc7ef4f98e927286ba37b5b82cf77d935559
Marcel Malewski <mmalewski9@gmail.com>
2025-04-12 17:47:13 +0200

    added bottom menu

---
+++ ui/src/app/component/bottom-menu/bottom-menu.component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, OnDestroy } from '@angular/core';
+import { CommandLineComponent } from '../command-line/command-line.component';
+import { Subject, takeUntil } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { Pages } from '../../other/typesAndConsts';
+import {
+    LoggedOutMessage,
+    UnknownServerErrorMessageRefreshPage,
+} from '../../other/message';
+import { Router } from '@angular/router';
+import { GeneralActionsService } from '../../service/general-actions.service';
+import { PrincipalDataService } from '../../service/principal-data.service';
+import { NotificationService } from '../../service/notification.service';
+
+@Component({
+    selector: 'bottom-menu',
+    standalone: true,
+    templateUrl: './bottom-menu.component.html',
+    imports: [CommandLineComponent],
+})
+export class BottomMenuComponent implements OnDestroy {
+    @Input({ required: true })
+    commandContext!: string;
+
+    private componentDestroyed$ = new Subject<void>();
+
+    constructor(
+        private router: Router,
+        private generalActionsService: GeneralActionsService,
+        private principalDataService: PrincipalDataService,
+        private notificationService: NotificationService
+    ) {}
+
+    ngOnDestroy(): void {
+        this.componentDestroyed$.next();
+        this.componentDestroyed$.complete();
+    }
+
+    submitLogout() {
+        this.generalActionsService
+            .logout()
+            .pipe(takeUntil(this.componentDestroyed$))
+            .subscribe({
+                error: (response: HttpResponse<any>) => {
+                    if (response.status === 401) {
+                        this.principalDataService.clearPrincipalData();
+                        this.router.navigateByUrl(Pages.LOGIN);
+                        this.notificationService.openSuccessNotification(
+                            LoggedOutMessage
+                        );
+                    } else {
+                        this.notificationService.openErrorNotification(
+                            UnknownServerErrorMessageRefreshPage
+                        );
+                        this.router.navigateByUrl(Pages.UNKNOWN_ERROR, {
+                            skipLocationChange: true,
+                        });
+                    }
+                },
+            });
+    }
+}
