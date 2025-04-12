ef3ecfe53f30176720a515772749257e5806a4bd
Marcel Malewski <mmalewski9@gmail.com>
2025-04-12 16:31:26 +0200

    timer focus works

--- ui/src/app/component/timer-focus/timer-focus.component.ts
+++ ui/src/app/component/timer-focus/timer-focus.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommandLineComponent } from '../command-line/command-line.component';
 
 @Component({
     selector: 'app-timer-focus',
     standalone: true,
     templateUrl: './timer-focus.component.html',
+    imports: [CommandLineComponent],
 })
 export class TimerFocusComponent {}
