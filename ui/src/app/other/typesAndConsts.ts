882211513acf601d73c401bf0c216082029151a1
Marcel Malewski <mmalewski9@gmail.com>
2025-06-08 12:06:57 +0200

    Update

--- ui/src/app/other/typesAndConsts.ts
+++ ui/src/app/other/typesAndConsts.ts
@@ -1,8 +1,7 @@
 export const Pages = {
     LOGIN: 'login',
     TIMER_HOME: 'timer',
-    TIMER_FOCUS: 'timer/focus',
-    TIMER_PAUSE: 'timer/pause',
+    TIMER_FOCUS: 'timer/session',
     TIMER_EDIT: 'timer/edit',
     SETTINGS: 'settings',
     UNKNOWN_ERROR: 'unknown-error',
