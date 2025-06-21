43cd8af8b113067e97e6391128a4e3e0b95f7e63
Marcel Malewski <mmalewski9@gmail.com>
2025-06-21 13:30:06 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalBasicDataDto.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalBasicDataDto.java
@@ -3,19 +3,20 @@ package com.marcel.malewski.focustimetrackerapi.entity.person.dto;
 import com.marcel.malewski.focustimetrackerapi.enums.Stage;
 
 public record PrincipalBasicDataDto(
-    Long id,
+    long id,
     String login,
     String email,
     Stage timerStage,
     String timerSelectedTopic,
-    Integer timerSetHours,
-    Integer timerSetMinutes,
-    Integer timerSetSeconds,
-    Integer timerShortBreak,
-    Integer timerLongBreak,
-    Boolean timerAutoBreak,
-    Boolean stopWatchAutoBreak,
-    Integer timerBreakTime,
-    Integer timerInterval
+    int timerSetHours,
+    int timerSetMinutes,
+    int timerSetSeconds,
+    int timerShortBreak,
+    int timerLongBreak,
+    boolean timerAutoBreak,
+    int timerInterval,
+    Integer timerRemainingFocus,
+    Integer timerRemainingInterval,
+    Integer timerRemainingBreak
 ) {
 }
