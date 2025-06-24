c09e72379ef25cdbda927b0e0df11cdd70c1f381
Marcel Malewski <mmalewski9@gmail.com>
2025-06-24 20:36:12 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/converter/StageConverter.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/converter/StageConverter.java
@@ -9,23 +9,23 @@ import java.util.stream.Stream;
 @Converter
 public class StageConverter implements AttributeConverter<Stage, String> {
 
-  @Override
-  public String convertToDatabaseColumn(Stage maybeStage) {
-    return switch (maybeStage) {
-      case null -> null;
-      case Stage stage -> stage.getStageName();
-    };
-  }
+    @Override
+    public String convertToDatabaseColumn(Stage maybeStage) {
+        return switch (maybeStage) {
+            case null -> null;
+            case Stage stage -> stage.getStageName();
+        };
+    }
 
-  // TODO stworzyc konkretniejszy exception
-  @Override
-  public Stage convertToEntityAttribute(String maybeStageName) {
-    return switch (maybeStageName) {
-      case null -> null;
-      case String stageName -> Stream.of(Stage.values())
-        .filter(s -> s.getStageName().equals(stageName))
-        .findFirst()
-        .orElseThrow(IllegalArgumentException::new);
-    };
-  }
+    // TODO słaby exception
+    @Override
+    public Stage convertToEntityAttribute(String maybeStageName) {
+        return switch (maybeStageName) {
+            case null -> null;
+            case String stageName -> Stream.of(Stage.values())
+                .filter(s -> s.getStageName().equals(stageName))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+        };
+    }
 }
