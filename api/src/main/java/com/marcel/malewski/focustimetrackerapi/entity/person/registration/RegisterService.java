c204b1462176aa73074a7cc14c1ee5545f1916b2
Marcel Malewski <mmalewski9@gmail.com>
2025-06-24 20:36:09 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/registration/RegisterService.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/registration/RegisterService.java
@@ -9,30 +9,28 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterService {
-  private final PersonService personService;
-  private final BCryptPasswordEncoder passwordEncoder;
+    private final PersonService personService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-  public RegisterService(PersonService personService, BCryptPasswordEncoder passwordEncoder) {
-    this.personService = personService;
-    this.passwordEncoder = passwordEncoder;
-  }
-
-
-  //TODO probably settings entity should be created here as well
-  public void register(RegisterRequestDto registerRequestDto) {
-    String login = registerRequestDto.getLogin();
-    if (personService.existsByLogin(login)) {
-      throw new LoginAlreadyUsedException(login);
+    public RegisterService(PersonService personService, BCryptPasswordEncoder passwordEncoder) {
+        this.personService = personService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    String email = registerRequestDto.getEmail();
-    if (personService.existsByEmail(email)) {
-      throw new EmailAlreadyUsedException(email);
+    public void register(RegisterRequestDto registerRequestDto) {
+        String login = registerRequestDto.getLogin();
+        if (personService.existsByLogin(login)) {
+            throw new LoginAlreadyUsedException(login);
+        }
+
+        String email = registerRequestDto.getEmail();
+        if (personService.existsByEmail(email)) {
+            throw new EmailAlreadyUsedException(email);
+        }
+
+        String encodedPassword = passwordEncoder.encode(registerRequestDto.getPassword());
+        Person newPerson = Person.builder().login(login).password(encodedPassword).email(email).build();
+
+        personService.create(newPerson);
     }
-
-    String encodedPassword = passwordEncoder.encode(registerRequestDto.getPassword());
-    Person newPerson = Person.builder().login(login).password(encodedPassword).email(email).build();
-
-    personService.create(newPerson);
-  }
 }
