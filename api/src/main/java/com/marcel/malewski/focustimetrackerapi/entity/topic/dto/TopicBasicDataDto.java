bac984d63f45fac65c4f0e79f64c9083191f7f83
Marcel Malewski <mmalewski9@gmail.com>
2025-06-24 20:36:24 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/topic/dto/TopicBasicDataDto.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/topic/dto/TopicBasicDataDto.java
@@ -1,10 +1,8 @@
 package com.marcel.malewski.focustimetrackerapi.entity.topic.dto;
 
-import com.marcel.malewski.focustimetrackerapi.entity.topic.interfaces.TopicBasicData;
-
 // TODO walidacja
 public record TopicBasicDataDto(
     Long id,
     String name
-) implements TopicBasicData {
+) {
 }
