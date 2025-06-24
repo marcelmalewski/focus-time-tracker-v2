5ace97340b95aa5a530d5ac9c67bd2a8e775d534
Marcel Malewski <mmalewski9@gmail.com>
2025-06-24 20:36:07 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalWithMainTopicsDto.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalWithMainTopicsDto.java
@@ -1,11 +1,11 @@
 package com.marcel.malewski.focustimetrackerapi.entity.person.dto;
 
-import com.marcel.malewski.focustimetrackerapi.entity.topic.interfaces.TopicBasicData;
+import com.marcel.malewski.focustimetrackerapi.entity.topic.dto.TopicBasicDataDto;
 
 import java.util.List;
 
 public record PrincipalWithMainTopicsDto(
     PrincipalBasicDataDto principalBasicData,
-    List<TopicBasicData> mainTopicsBasicData
+    List<TopicBasicDataDto> mainTopicsBasicData
 ) {
 }
