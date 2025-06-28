94656bbe1c8d4db95aaab830dfe5e7aebd7d30e3
Marcel Malewski <mmalewski9@gmail.com>
2025-06-28 11:13:23 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/security/exception/InvalidPasswordException.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/security/exception/InvalidPasswordException.java
@@ -3,9 +3,10 @@ package com.marcel.malewski.focustimetrackerapi.security.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// TODO jak nie potrzebny to wyrzucić
 @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class InvalidPasswordException extends RuntimeException {
-  public InvalidPasswordException() {
-    super("Invalid password exception");
-  }
+    public InvalidPasswordException() {
+        super("Invalid password exception");
+    }
 }
