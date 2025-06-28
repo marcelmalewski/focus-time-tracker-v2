15ecd8a40016761c38060bea43f4e73e03727b79
Marcel Malewski <mmalewski9@gmail.com>
2025-06-28 11:13:36 +0200

    Update

--- ui/src/app/component/register/register.component.ts
+++ ui/src/app/component/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+// TODO rejestracja na potem
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
+    selector: 'app-register',
+    standalone: true,
+    imports: [],
+    templateUrl: './register.component.html',
 })
 export class RegisterComponent {}
