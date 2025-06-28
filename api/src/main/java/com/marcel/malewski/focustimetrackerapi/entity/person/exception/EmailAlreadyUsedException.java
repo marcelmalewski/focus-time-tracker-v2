62b3ecb4e7f5a917c95fdf2235a3ad26d6f254d2
Marcel Malewski <mmalewski9@gmail.com>
2025-06-28 11:13:19 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/exception/EmailAlreadyUsedException.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/exception/EmailAlreadyUsedException.java
@@ -3,9 +3,10 @@ package com.marcel.malewski.focustimetrackerapi.entity.person.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// TODO branch test, rejestracja
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class EmailAlreadyUsedException extends RuntimeException {
-  public EmailAlreadyUsedException(String email) {
-    super("Another person already uses this email, email: " + email);
-  }
+    public EmailAlreadyUsedException(String email) {
+        super("Another person already uses this email, email: " + email);
+    }
 }
