df0b79b5fc0fafc70e7388b579560f5ab5470d26
Marcel Malewski <mmalewski9@gmail.com>
2025-06-30 20:35:59 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/TimerBreakDto.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/TimerBreakDto.java
@@ -1,6 +1,13 @@
 package com.marcel.malewski.focustimetrackerapi.entity.person.dto;
 
+import com.marcel.malewski.focustimetrackerapi.enums.BreakType;
+import jakarta.validation.constraints.NotNull;
+
 public record TimerBreakDto(
+    BreakType breakTypeToStart,
+    Integer timerRemainingInterval,
+    @NotNull
+    boolean timerAutoBreak
 ) {
 }
 
