b38b82fe927412a7651d1d3bb457781c0d6dc924
Marcel Malewski <mmalewski9@gmail.com>
2025-09-06 11:58:13 +0200

    Update

--- api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalBasicDataDto.java
+++ api/src/main/java/com/marcel/malewski/focustimetrackerapi/entity/person/dto/PrincipalBasicDataDto.java
@@ -1,12 +1,17 @@
 package com.marcel.malewski.focustimetrackerapi.entity.person.dto;
 
 import com.marcel.malewski.focustimetrackerapi.enums.Stage;
+import com.marcel.malewski.focustimetrackerapi.validation.space.WithoutSpacesOnTheSides;
+import com.marcel.malewski.focustimetrackerapi.validation.space.WithoutSpacesWithinAWord;
 import jakarta.validation.constraints.NotNull;
 
 public record PrincipalBasicDataDto(
-    @NotNull
-    long id,
+    Long id,
+    @WithoutSpacesWithinAWord
+    @WithoutSpacesOnTheSides
     String login,
+    @WithoutSpacesWithinAWord
+    @WithoutSpacesOnTheSides
     String email,
     Stage timerStage,
     String timerSelectedTopic,
